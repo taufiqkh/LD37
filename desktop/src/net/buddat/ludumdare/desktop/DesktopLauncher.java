057bd2d739d892943529bc07b89da32a077d69e1
Taufiq Hoven <taufiq.hoven@gmail.com>
2016-12-10 11:58:49 +1000

    Initial commit

---
+++ desktop/src/net/buddat/ludumdare/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.buddat.ludumdare.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.buddat.ludumdare.LD37;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD37(), config);
+	}
+}
---
+++ html/src/net/buddat/ludumdare/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package net.buddat.ludumdare.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import net.buddat.ludumdare.LD37;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new LD37();
+        }
+}
