eea2d5e460c331516f67901e27a7d896e99aa05e
Taufiq Hoven <taufiq.hoven@gmail.com>
2016-12-10 14:33:55 +1000

    Add basic horizontal/vertical walls

--- core/src/net/buddat/ludumdare/LogicEngine.kt
+++ core/src/net/buddat/ludumdare/LogicEngine.kt
@@ -1,12 +1,18 @@
 package net.buddat.ludumdare
 
 import com.badlogic.ashley.core.Engine
+import net.buddat.ludumdare.entity.PlayerEntity
+import net.buddat.ludumdare.entity.Room
 
 /**
  * Powers the entity handling
  */
 class LogicEngine : Runnable {
     val engine: Engine = Engine()
+    init {
+        engine.addEntity(Room())
+        engine.addEntity(PlayerEntity())
+    }
 	
 	override fun run() {
 		println("Hello World")
--- core/src/net/buddat/ludumdare/entity/Room.kt
+++ core/src/net/buddat/ludumdare/entity/Room.kt
@@ -3,4 +3,5 @@ package net.buddat.ludumdare.entity
 import com.badlogic.ashley.core.Entity
 
 class Room : Entity() {
+    val walls: MutableList<Wall> = mutableListOf()
 }
