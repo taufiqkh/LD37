377779753dd2282ffc8832a55720ec90c0042436
Taufiq Hoven <taufiq.hoven@gmail.com>
2016-12-10 20:56:14 +1000

    Using Constants.INSTANCE instead of ConstantsKt

--- desktop/src/net/buddat/ludumdare/desktop/DesktopLauncher.java
+++ desktop/src/net/buddat/ludumdare/desktop/DesktopLauncher.java
@@ -2,7 +2,7 @@ package net.buddat.ludumdare.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import net.buddat.ludumdare.ConstantsKt;
+import net.buddat.ludumdare.Constants;
 import net.buddat.ludumdare.LD37;
 
 public class DesktopLauncher {
@@ -10,13 +10,13 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		
-		config.width = ConstantsKt.getWidth();
-		config.height = ConstantsKt.getHeight();
+		config.width = Constants.INSTANCE.getWidth();
+		config.height = Constants.INSTANCE.getHeight();
 		config.resizable = false;
-		config.title = ConstantsKt.getTitle();
+		config.title = Constants.INSTANCE.getTitle();
 		config.x = -1;
 		config.y = -1;
-		config.foregroundFPS = ConstantsKt.getFPS();
+		config.foregroundFPS = Constants.INSTANCE.getFPS();
 		
 		new LwjglApplication(new LD37(), config);
 	}
