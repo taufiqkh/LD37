7ae275a44b69382d1082fc81719795a4c93617a9
BuddaT <budda13@gmail.com>
2016-12-10 12:07:44 +0800

    Initial window setup and constants file.

---
+++ core/src/net/buddat/ludumdare/Constants.kt
@@ -0,0 +1,8 @@
+package net.buddat.ludumdare
+
+public val width = 800
+public val height = 600
+
+public val FPS = 90
+
+public val title = "Ludum Dare 37 Game"
--- desktop/src/net/buddat/ludumdare/desktop/DesktopLauncher.java
+++ desktop/src/net/buddat/ludumdare/desktop/DesktopLauncher.java
@@ -2,11 +2,23 @@ package net.buddat.ludumdare.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.buddat.ludumdare.ConstantsKt;
 import net.buddat.ludumdare.LD37;
 
 public class DesktopLauncher {
+	
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		config.width = ConstantsKt.getWidth();
+		config.height = ConstantsKt.getHeight();
+		config.resizable = false;
+		config.title = ConstantsKt.getTitle();
+		config.x = -1;
+		config.y = -1;
+		config.foregroundFPS = ConstantsKt.getFPS();
+		
 		new LwjglApplication(new LD37(), config);
 	}
+	
 }
