50c0344df2532378aede81ba07d2674f1aae1bcb
Taufiq Hoven <taufiq.hoven@gmail.com>
2016-12-10 15:14:57 +1000

    Added basic key mapping

---
+++ core/src/net/buddat/ludumdare/input/InputHandler.kt
@@ -0,0 +1,14 @@
+package net.buddat.ludumdare.input
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
+
+/**
+ * Checks relevant input for provision eg. to a movement controller
+ */
+class InputHandler(val keymapping: Keymapping) {
+	fun retrieveInput() {
+
+	}
+}
+
